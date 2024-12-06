015babe0c7362a5704e9d00c3b20004ed6bd6fac
sergwest <sergwest585@gmail.com>
2024-12-06 13:02:46 +0300

    create interface for program

---
+++ files.h
@@ -0,0 +1,10 @@
+
+#ifndef FILES_H
+#define FILES_H
+
+#include <stdbool.h>
+
+bool is_file_exist(const char *path);
+bool is_directory(const char *path);
+
+#endif
---
+++ ssed.h
@@ -0,0 +1,13 @@
+
+#ifndef SSED_H
+#define SSED_H
+
+
+void sed_parse_cmd(char *filename, char *cmd);
+void sed_replace(char *filename, char *before, char *after);
+void sed_delete(char *filename, char *pattern);
+void sed_add_suffix(char *filename, char *text);
+void sed_add_prefix(char *filename, char *text);
+
+
+#endif
